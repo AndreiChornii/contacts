89f787f02f68e10078401d95a4d6bd3cfc4d71c9
AndreiChornii <andrei.chornii@gmail.com>
2020-02-26 12:16:47 +0200

    ready_links

--- server/views/header.php
+++ server/views/header.php
@@ -13,20 +13,20 @@
     <header class="header">
         <nav>
             <ul class="header__menu">
-                <?php if(!empty($currentUser['username'])){ ?>
-                <li><a class="header__link" href="/">Contacts</a></li>
+                <?php if(!empty($currentUser['username'])){?>
+                <li><a class="header__link<?php if($route === '/') {echo ' active';} ?>" href="/">Contacts</a></li>
                 <?php } ?>
                 <?php if(!empty($currentUser['username'])){ ?>
                 <!--<li><a class="header__link" href="/email">Email</a></li>-->
                 <?php } ?>
-                <li><a class="header__link" href="/login">Login</a></li>
+                <li><a class="header__link<?php if($route === '/login') {echo ' active';} ?>" href="/login">Login</a></li>
                 <?php if($isAdmin) { ?>
-                <li><a class="header__link" href="/users">Users</a></li>
+                <li><a class="header__link<?php if($route === '/users') {echo ' active';} ?>" href="/users">Users</a></li>
                 <?php } ?>
-                <li><a class="header__link" href="/registration">Registration</a></li>
+                <li><a class="header__link<?php if($route === '/registration') {echo ' active';} ?>" href="/registration">Registration</a></li>
                 <?php if(!empty($currentUser['username'])){ ?>
-                <li><a class="header__link" href="/contacts">Favorite contacts</a></li>
-                <li><a class="header__link" href="/logout">Logout</a></li>
+                <li><a class="header__link<?php if($route === '/contacts') {echo ' active';} ?>" href="/contacts">Favorite contacts</a></li>
+                <li><a class="header__link<?php if($route === '/logout') {echo ' active';} ?>" href="/logout">Logout</a></li>
                 <?php } ?>
                 <li><b><?= $currentUser['username'] ?></b></li>
             </ul>
