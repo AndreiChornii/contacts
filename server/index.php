f1404138cabe7e9c4623a048e5d30b0babb1fbcc
AndreiChornii <andrei.chornii@gmail.com>
2020-06-11 16:38:30 +0300

    index_2

--- server/index.php
+++ server/index.php
@@ -1,17 +1,18 @@
 <?php
-session_start();
-include './includes/DatabaseConnection.php';
-include './handlers/dump.php';
-include './handlers/mysqli.php';
-include './handlers/validate.php';
-
-$route = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
-
-echo $route;
-echo '<BR />';
-echo $method;
-
-include './handlers/routesMethodGet.php';
-
-include './handlers/routesMethodPost.php';
+echo 'hello_world';
+//session_start();
+//include './includes/DatabaseConnection.php';
+//include './handlers/dump.php';
+//include './handlers/mysqli.php';
+//include './handlers/validate.php';
+//
+//$route = $_SERVER['REQUEST_URI'];
+//$method = $_SERVER['REQUEST_METHOD'];
+//
+//echo $route;
+//echo '<BR />';
+//echo $method;
+//
+//include './handlers/routesMethodGet.php';
+//
+//include './handlers/routesMethodPost.php';
