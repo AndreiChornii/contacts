ae43c0a13bb05c33f4cdfd8a2801795271e2b9c6
AndreiChornii <andrei.chornii@gmail.com>
2020-06-11 16:32:43 +0300

    index

--- server/index.php
+++ server/index.php
@@ -8,6 +8,10 @@ include './handlers/validate.php';
 $route = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+echo $route;
+echo '<BR />';
+echo $method;
+
 include './handlers/routesMethodGet.php';
 
 include './handlers/routesMethodPost.php';
