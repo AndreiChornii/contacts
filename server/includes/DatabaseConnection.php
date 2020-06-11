e1f153dcf9f0c079a60791fe2191f394736fbc60
AndreiChornii <andrei.chornii@gmail.com>
2020-06-11 15:20:10 +0300

    gitig

--- server/includes/DatabaseConnection.php
+++ server/includes/DatabaseConnection.php
@@ -1,4 +1,5 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=form_php;charset=utf8', 'ijdb_sample', 'mypassword');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 $DB = mysqli_connect("localhost", "ijdb_sample", "mypassword", "form_php");
